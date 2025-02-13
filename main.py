5694a9c0e0f8e3d8fc88f4182e4d5c28445f2561
Milomeli62 <a01368105@tec.mx>
2025-02-13 16:15:36 -0600

    Calculo: Area y perimetro
    -main.py: input del diametro, output area y perimetro

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ perimetro = 2 * math.pi * r
 area = math.pi * r ** 2
 
 print(f"Perímetro: {perimetro:.2f}, Área: {area:.2f}")
+
+volumen = (4/3) * math.pi * r ** 3
+
+print(f"Volumen: {volumen:.2f}")
